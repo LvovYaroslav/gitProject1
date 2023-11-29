470228d15659938fa9fc84f711dfbae1f6c0e010
LvovYaroslav <152399390+LvovYaroslav@users.noreply.github.com>
2023-11-29 23:16:46 +1000

    Update program.py
    the final one

--- program.py
+++ program.py
@@ -1,11 +1,10 @@
 def main():
     print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
     main()
-
-a = 123
-b = 456
-c = 789
-
+    
